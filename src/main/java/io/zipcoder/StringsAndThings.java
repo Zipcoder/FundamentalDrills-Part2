1e3da3b14da37a76dca41b4f09f95372f5ec010f
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-02-21 14:42:52 -0500

    part 2

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -7,47 +7,68 @@ public class StringsAndThings {
      * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
-        return null;
+    public Integer countYZ(String input) {
+
+        String[] words = input.split(" ");
+        Integer count = 0;
+
+        for(Integer i = 0; i < words.length; i++){
+            if(words[i].endsWith("y") || words[i].endsWith("z")){
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
      * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
+     * <p>
      * example : withoutString("Hello there", "llo") // Should return "He there"
-     *           withoutString("Hello there", "e") //  Should return "Hllo thr"
-     *           withoutString("Hello there", "x") // Should return "Hello there"
+     * withoutString("Hello there", "e") //  Should return "Hllo thr"
+     * withoutString("Hello there", "x") // Should return "Hello there"
      */
-    public String withoutString(String base, String remove){
-        return null;
+    public String withoutString(String base, String remove) {
+
+        return base.replaceAll(remove, "");
     }
 
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
      * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
+     * <p>
      * example : equalIsNot("This is not")  // Should return false
-     *           equalIsNot("This is notnot") // Should return true
-     *           equalIsNot("noisxxnotyynotxisi") // Should return true
+     * equalIsNot("This is notnot") // Should return true
+     * equalIsNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean equalIsNot(String input){
-        return null;
+    public Boolean equalIsNot(String input) {
+
+        int isCounter = input.split("is", -1).length - 1;     // -1 get rid of empty space & keeps count correct
+        int notCounter = input.split("not", -1).length - 1;
+
+        if(isCounter == notCounter){
+            return true;
+        }
+        return false;
     }
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
      * Return true if all the g's in the given string are happy.
      * example : gHappy("xxggxx") // Should return  true
-     *           gHappy("xxgxx") // Should return  false
-     *           gHappy("xxggyygxx") // Should return  false
+     * gHappy("xxgxx") // Should return  false
+     * gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
-        return null;
+    public Boolean gIsHappy(String input) {
+
+        if(input.contains("gg")){
+            return true;
+        }
+        return false;
     }
 
 
@@ -55,10 +76,19 @@ public class StringsAndThings {
      * We'll say that a "triple" in a string is a char appearing three times in a row.
      * Return the number of triples in the given string. The triples may overlap.
      * example :  countTriple("abcXXXabc") // Should return 1
-     *            countTriple("xxxabyyyycd") // Should return 3
-     *            countTriple("a") // Should return 0
+     * countTriple("xxxabyyyycd") // Should return 3
+     * countTriple("a") // Should return 0
      */
-    public Integer countTriple(String input){
-        return null;
+    public Integer countTriple(String input) {
+
+        int counter = 0;
+        int newInput = input.length() - 2; // to avoid out of bounds error
+
+        for( int i = 0; i < newInput; i++){
+            if(input.charAt(i) == input.charAt(i + 1) && input.charAt(i) == input.charAt(i + 2)){
+                counter++;
+            }
+        }
+        return counter;
     }
 }
