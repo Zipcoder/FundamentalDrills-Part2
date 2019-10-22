e41d244526dda05f750c12363b7303bf472b2f60
wes <wes.jones1597@gmail.com>
2019-10-22 13:31:45 -0400

    removeString tests passing

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -39,7 +39,7 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+        return base.replace(remove, "");
     }
 
     /**
