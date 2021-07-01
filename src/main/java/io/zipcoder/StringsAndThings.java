7b07d3a3eac06462cfd613e7e783c86767b4c541
Manny <manny@ZCW-JLAT.home>
2021-07-01 08:39:49 -0400

    Manny finished StringsAndThings

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -131,9 +131,7 @@ public class StringsAndThings {
 //            for (int j = i + 1; j < input.length(); j++) {
 //                if (input.charAt(i) == input.charAt(j)) {
 //                    return true;
-//                }
-//            }
-//        }
+//
 
     /**
      * We'll say that a "triple" in a string is a char appearing three times in a row.
@@ -144,7 +142,7 @@ public class StringsAndThings {
      */
     public Integer countTriple(String input){
         int count = 0;
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < input.length() - 1; i++) {
             if (input.charAt(i) == input.charAt(i + 1)) {
                 if (input.charAt(i + 1) == input.charAt(i + 2)) {
                     count ++;
